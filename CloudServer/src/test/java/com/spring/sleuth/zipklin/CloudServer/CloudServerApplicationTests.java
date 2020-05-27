1958d146d2551b037e6b4e216cfc33a1ce6c4b23
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-27 12:57:57 +0530

    Sleuth Zipkin Test

---
+++ CloudServer/src/test/java/com/spring/sleuth/zipklin/CloudServer/CloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sleuth.zipklin.CloudServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
