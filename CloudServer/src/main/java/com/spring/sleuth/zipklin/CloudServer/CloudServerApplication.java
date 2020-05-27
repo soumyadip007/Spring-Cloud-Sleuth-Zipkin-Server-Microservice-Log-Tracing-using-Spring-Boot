d25d7f0b9eff5cad2d355bf6df1070891b24bdb3
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-27 12:57:53 +0530

    Sleuth Zipkin Main

---
+++ CloudServer/src/main/java/com/spring/sleuth/zipklin/CloudServer/CloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sleuth.zipklin.CloudServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerApplication.class, args);
+	}
+
+}
