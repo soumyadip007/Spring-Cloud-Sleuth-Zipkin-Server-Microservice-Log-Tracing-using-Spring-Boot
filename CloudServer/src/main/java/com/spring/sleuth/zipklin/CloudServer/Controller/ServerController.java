de9d4027bb85a9a57911bcdf5e96ba8c5cdd2506
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-27 13:17:23 +0530

    Controller added

---
+++ CloudServer/src/main/java/com/spring/sleuth/zipklin/CloudServer/Controller/ServerController.java
@@ -0,0 +1,20 @@
+package com.spring.sleuth.zipklin.CloudServer.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class ServerController {
+
+	private static final Logger log=LoggerFactory.getLogger(ServerController.class);
+	
+	@GetMapping(value = "/server")
+    public String hello() {
+		log.info("Reached Server");
+        return "HelloWorld";
+    }
+	
+}
