4e65ba9b126df3354eecd37e2e470b6b6a662756
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-27 13:28:10 +0530

    Component Scan Entity Scan added

--- CloudServer/src/main/java/com/spring/sleuth/zipklin/CloudServer/CloudServerApplication.java
+++ CloudServer/src/main/java/com/spring/sleuth/zipklin/CloudServer/CloudServerApplication.java
@@ -2,8 +2,14 @@ package com.spring.sleuth.zipklin.CloudServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@ComponentScan({"com.spring.sleuth.zipklin.*"})
+@EntityScan("com.spring.sleuth.zipklin.*")
 public class CloudServerApplication {
 
 	public static void main(String[] args) {
