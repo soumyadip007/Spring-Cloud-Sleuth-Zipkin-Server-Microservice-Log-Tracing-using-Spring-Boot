212c664aac66e90ed8a758eff8c604f6763486f5
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-27 13:17:50 +0530

    Seluth Zipkin Server

--- CloudServer/src/main/java/com/spring/sleuth/zipklin/CloudServer/Controller/ServerController.java
+++ CloudServer/src/main/java/com/spring/sleuth/zipklin/CloudServer/Controller/ServerController.java
@@ -2,6 +2,7 @@ package com.spring.sleuth.zipklin.CloudServer.Controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class ServerController {
 	@GetMapping(value = "/server")
     public String hello() {
 		log.info("Reached Server");
-        return "HelloWorld";
+        return "Hi! This is Sleuth-Zipkin Server";
     }
 	
 }
